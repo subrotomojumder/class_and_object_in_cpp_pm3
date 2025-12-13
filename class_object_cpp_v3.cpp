39d1cd0d172275ae99a0f9951608a617c9dd048a
subrotomojumder <subrotomojumder03@gmail.com>
2025-12-13 22:54:29 +0600

    first commit

---
+++ class_object_cpp_v3.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student
+{
+public:
+    char name[100];
+    int roll;
+    double gpa;
+};
+
+int main()
+{
+    Student a;
+    a.name = "Bijoy";
+    a.roll = 10;
+    a.gpa = 3.5;
+    return 0;
+}
